79f16f598d58dbf676a7d716bbfb4d158e515ad2
benmday <benjaminmichaelday@gmail.com>
2022-12-02 13:20:25 -0800

    Created database and connected successfully

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS directory_db;
+CREATE DATABASE directory_db;
+
+USE directory_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE,
+
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+USE directory_db;
+
+INSERT INTO departments (name)
+VALUES 
+('Sales'),
+('HR'),
+('Customer Support');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Salesperson', 65000, 1),
+('Sales Lead', 80000, 1),
+('Support Rep', 60000, 3),
+('Support Lead', 75000, 3),
+('HR Manager', 100000, 2),
+('CEO', 150000, NULL);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+('Courtney', 'Brown', 4),
+('Rex', 'Smith', 1),
+('Kevin', 'Kline', 4),
+('Angela', 'Lansbury', 6),
+('Linda', 'Ronstadt', 3),
+('George', 'Rose', 5),
+('Tony', 'Azito', 2);
